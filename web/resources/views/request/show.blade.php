a9ed1938cf6a7d66d0c2afc74783726f7e67f94b
Tomas Bedrich <ja@tbedrich.cz>
2015-12-07 16:27:15 +0100

    Updated request detail info panel

--- web/resources/views/request/show.blade.php
+++ web/resources/views/request/show.blade.php
@@ -7,7 +7,7 @@
 
 <h1 class="page-header">{{ $request->template->name }} #{{ $request->id }} <small>(request)</small></h1>
 
-<div class="row">	
+<div class="row">
 	<div class="col-xs-12 col-sm-8">
 		<div class="panel panel-default">
 			<div class="panel-heading">Request data</div>
@@ -15,43 +15,40 @@
 				<div id="document-tree">
 				</div>
 			</div>
-		</div>		
+		</div>
 	</div>
-	
+
 	<div class="col-xs-12 col-sm-4">
 		<div class="panel panel-default">
 			<div class="panel-heading">Info</div>
-			<div class="panel-body">
-				<ul class="list-group">
-					<li class="list-group-item">
-						<span class="pull-right">
-							@if (!$request->template->deleted_at)
-								<a href="{{ action('TemplateController@show', $request->template->id) }}" class="btn btn-xs btn-link">{{ $request->template->name }}</a>
-							@else
-								<span class="text-muted"><s>{{ $request->template->name }}</s></span>
-							@endif
-						</span>
-						
-						Template
-					</li>
-					
-					<li class="list-group-item">
-						<span class="pull-right">{{ $request->created_at }}</span>
-						Created
-					</li>
-					
-					<li class="list-group-item">
-						<span class="pull-right">@include('partial.request_status', ['request' => $request])</span>
-						Status
-					</li>
-					@if ($request->status == App\Request::STATUS_DONE)
-					<li class="list-group-item">
-						<span class="pull-right"><a href="{{action('RequestController@download', ['request' => $request->id ])}}"><i class="glyphicon glyphicon-floppy-save"></i> Package <small>{{ $archive_size }}</small></a></span>
-						Download
-					</li>
-					@endif
-				</ul>
-			</div>
+			<table class="table">
+				<tr>
+					<th>Template</th>
+					<td>
+						@if (!$request->template->deleted_at)
+							<a href="{{ action('TemplateController@show', $request->template->id) }}">{{ $request->template->name }}</a>
+						@else
+							<span class="text-muted"><s>{{ $request->template->name }}</s></span>
+						@endif
+					</td>
+				</tr>
+
+				<tr>
+					<th>Created</th>
+					<td>{{ $request->created_at }}</td>
+				</tr>
+
+				<tr>
+					<th>Status</th>
+					<td>@include('partial.request_status', ['request' => $request])</td>
+				</tr>
+				@if ($request->status == App\Request::STATUS_DONE)
+				<tr>
+					<th>Download</th>
+					<td><a href="{{action('RequestController@download', ['request' => $request->id ])}}"><i class="glyphicon glyphicon-floppy-save"></i> Package <small>{{ $archive_size }}</small></a></td>
+				</tr>
+				@endif
+			</table>
 		</div>
 	</div>
 </div>
@@ -61,21 +58,21 @@
 
 @endsection
 
-		
+
 @section('custom_scripts')
 <script type="text/javascript">
 	//@TODO: Convert this to interactive tree
-	
+
 	var data = {!! $request->data !!},
 		$target = $("#document-tree");
-			
+
 	function make_tree(data, prefix) {
 		var html = "<ul>";
 		for(var i in data) {
 			var item = data[i];
 			if (prefix)
 				i = prefix + i;
-			
+
 			html += "<li><span class='var'>" + i + "</span>";
 			if (typeof item === typeof 0 || typeof item === typeof "") {
 				html += "<span class='value pull-right'>" + item + "</span>";
@@ -87,7 +84,7 @@
 		html += "</ul>";
 		return html;
 	}
-	
+
 	$target.append(make_tree(data, 'document'));
 </script>
 @endsection
